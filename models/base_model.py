cc0e80116e802841ab990c1626586c39d70b3589
Yunju Chen <info@yunjuchen.net>
2018-02-10 02:52:49 +0000

    Add #3, to_dict() incomplete

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 '''Base class model'''
 import uuid
-from datetime import datetime
+import datetime
 
 
 class BaseModel():
     '''Base class of all models'''
 
-    id=str(uuid.uuid4())
-    created_at=datetime.now()
-    updated_at=datetime.now()
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         '''Instance instantiation'''
-
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         '''__str__ representation'''
-
+        string = '['+self.__class__.__name__+']'+' ('+self.id+') ' +\
+                 str(self.__dict__)
+        return string
 
     def save(self):
-        ''' '''
-
+        '''Update updated_at time'''
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        '''Save __dict__ to a dictionary'''
+        '''Save __dict__ to a dict with class info'''
+        setattr(self, " __class__", str(type(self).__name__))
+        return self.__dict__
+
