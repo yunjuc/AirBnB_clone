2af6d6ab780562b053636dc2460654ad6798f2fe
Yunju Chen <info@yunjuchen.net>
2018-02-10 19:00:50 +0000

    #4 complete

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ import datetime
 class BaseModel():
     '''Base class of all models'''
 
-    def __init__(self):
+    def __init__(self, *arags, **kwargs):
         '''Instance instantiation'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k is 'created_at' or k is 'updated_at':
+                    setattr(self, k, datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif k is '__class__':
+                    pass 
+                else:
+                    setattr(self, k, v)
 
     def __str__(self):
         '''__str__ representation'''
@@ -27,7 +35,6 @@ class BaseModel():
         '''Serialize __dict__ and add class info'''
         j_dict = self.__dict__
         j_dict["updated_at"] = self.updated_at.isoformat()
-        j_dict["created_at"] = self.created_at.isoformat() 
+        j_dict["created_at"] = self.created_at.isoformat()
         j_dict["__class__"] = self.__class__.__name__
         return j_dict
-
