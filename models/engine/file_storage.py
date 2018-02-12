b2215c12bb54fe2be942aa256c4554d4e828efd9
Yunju Chen <info@yunjuchen.net>
2018-02-12 04:01:37 +0000

    Check-in code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
-''' '''
+"""
+FileStorage serializes and deserializes
+"""
+
+
+import json
+import models
+
+class FileStorage():
+    """
+    serializes and deserializes
+    """
+
+    __file_path = "file.json"
+    __object = {}
+
+    def all(self):
+        """
+        returns filestorage objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        sets __objects with key class name.id
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
