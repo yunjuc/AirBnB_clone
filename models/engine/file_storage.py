ff6796a326d76617aa8b9c926272291bffd9434a
imperfectskillz <j.choi.89@gmail.com>
2018-02-12 06:37:55 +0000

    need to complete reload method and update models/base_model.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,4 +28,15 @@ class FileStorage():
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
-    
+    def save(self):
+        """
+        serializes objects to JSON
+        """
+        with open(self.__file_path, mode='w', encoding='utf-8') as file:
+            file.write(json.dump(self.__objects))
+
+    def reload(self):
+        """
+        deserializes JSON file to __objects
+        """
+        
