c3d8fa4e1a59e5531664959725a7230c381d0f2c
Yunju Chen <info@yunjuchen.net>
2018-02-13 04:21:02 +0000

    Get updated code

--- console.py
+++ console.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 '''Command console interpreter'''
 import cmd
+import models
 from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
     '''hbnb console'''
     prompt = '(hbnb) '
-    cls_list = ['BaseModel']
+    cls_list = ['BaseModel', 'User', 'City', 'State', 'Place', 'Review', 
+                'Amenity']
 
     def do_quit(self, line):
         '''Quit command to exit the program
@@ -34,52 +42,55 @@ class HBNBCommand(cmd.Cmd):
         elif (arg[0] in cls_list) is False:
             print("** class doesn't exist **")
         else:
-            new = BaseModel(0)  # replace with save to file later
+            new = arg[0]()
+            models.storage.save()
             print(new.id)
 
     def do_show(self, line):
         '''Display instance info based on class name and id
         '''
         arg = line.split()
+        obj_list = models.storage.all()
         if len(arg) == 0:
             print("** class name missing **")
         elif (arg[0] in cls_list) is False:
             print("** class doesn't exist **")
         elif (arg[1] is None):
             print("** instance id missing **")
-        elif (arg[1] in json_file) is False:  # replace json_file
+        elif (arg[0]+'.'+arg[1] in obj_list) is False:
             print("** no instance found **")
         else:
-            for i in json_file:  # replace json_file
-                print(i.id)
+            print(obj_list[arg[0]+'.'+arg[1]])
 
     def do_destory(self, line):
         '''Delete instance based on class name and id
         '''
         arg = line.split()
+        obj_list = models.storage.all()
         if len(arg) == 0:
             print("** class name missing **")
         elif (arg[0] in cls_list) is False:
             print("** class doesn't exist **")
         elif (arg[1] is None):
             print("** instance id missing **")
-        elif (arg[1] in json_file) is False:  # replace json_file
+        elif (arg[0]+'.'+arg[1] in obj_list) is False:
             print("** no instance found **")
         else:
-            for i in json_file:  # replace json_file
-                del(i.id)       # check how to delete
+            del obj_list[arg[0]+'.'+arg[1]]
+            models.storage.save()
 
     def do_all(self, line):
         '''Display all instances info based on class name
         '''
         arg = line.split()
+        obj_list = models.storage.all()
         if len(arg) == 0:
             print("** class name missing **")
         elif (arg[0] in cls_list) is False:
             print("** class doesn't exist **")
         else:
-            for i in json_file:  # replace json_file
-                print(i.id)
+            for k, v in obj_list.items:  # doesn't work with seperate class name
+                print(v)
 
     def do_update(self, line):
         '''Update an instance based on class name and id with \
@@ -89,20 +100,21 @@ class HBNBCommand(cmd.Cmd):
            "<attribute value>"
         '''
         arg = line.split()
+        obj_list = models.storage.all()
         if len(arg) == 0:
             print("** class name missing **")
         elif (arg[0] in cls_list) is False:
             print("** class doesn't exist **")
         elif (arg[1] is None):
             print("** instance id missing **")
-        elif (arg[1] in json_file) is False:  # replace json_file
+        elif (arg[0]+'.'+arg[1] in obj_list) is False:
             print("** no instance found **")
         elif (arg[2] is None):
             print("** attribute name missing **")
         elif (arg[3] is None):
             print("** value missing **")
         else:
-            # update json_file
+            cls = obj_list[arg[0]]
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
